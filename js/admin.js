cabd8f95247a6a5efd3b3360a0e8116325937d67
KauamedeirosHonorato <kauamedeirosmh@gmail.com>
2025-11-13 16:10:57 -0300

    atualizando Rotas e GeoLocalizacao

--- js/admin.js
+++ js/admin.js
@@ -266,14 +266,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 deliveryMarker.setLatLng(latLng);
             } else {
                 deliveryMarker = L.marker(latLng, {
-                    icon: L.icon({
-                        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png',
-                        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-                        iconSize: [25, 41],
-                        iconAnchor: [12, 41],
-                        popupAnchor: [1, -34],
-                        shadowSize: [41, 41]
-                    })
+                  icon: L.icon({
+                    iconUrl: "./CarroIcone/Versa2025.png", // Caminho para sua imagem local
+                    iconSize: [70, 70], // Tamanho pequeno e nítido, estilo Waze
+                    iconAnchor: [17, 35], // Ponto do ícone que corresponde à localização
+                    popupAnchor: [1, -34], // Posição do popup em relação ao ícone
+                  }),
                 }).addTo(map);
             }
             deliveryMarker.bindPopup("Localização do Alexandre").openPopup();
